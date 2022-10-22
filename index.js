1955a8aa74297993aaeaafdaf78495628918e196
SerhiiIkin <messileonl@gmail.com>
2022-10-22 14:06:28 +0200

    little change

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ import commentsRouter from "./routes/comments.routes.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { sendMyEmail } from "./utils/sendEmail.js";
+import path from "path";
+import sendEmailRouter from "./routes/sendEmail.routes.js"
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 6000;
+const __dirname = path.dirname("")
 
 app.use(cors());
 app.use(express.json());
@@ -19,33 +21,10 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRouter);
 app.use(productsRouter);
 app.use(commentsRouter);
+app.use(sendEmailRouter);
 
-app.get("/", (req, res) => {
-    res.send(`
-    <p>For send mail use: "/api/sendemail"</p>
-    <p>For look list of products: <a href="/products">products</a></p>
-    <p>For look list of comments: <a href="/comments">comments</a></p>
-    `);
-});
+app.use(express.static(path.join(__dirname,'public')))
 
-app.post("/api/sendemail", async (req, res) => {
-    const { email, title, text } = req.body;
-
-    try {
-        const send_to = process.env.EMAIL_TO;
-        const send_from = process.env.EMAIL_FROM;
-        const reply_to = process.env.EMAIL_TO;
-        const subject = title;
-        const message = `<p>${text}</p>
-        Message from: ${email}`;
-
-        await sendMyEmail(subject, message, send_to, send_from, reply_to);
-
-        res.status(200).json({ success: true, message: "Email send" });
-    } catch (error) {
-        res.status(500).json({ success: false, message: error });
-    }
-});
 
 async function start() {
     await mongoose.connect(process.env.dbURL);
---
+++ routes/sendEmail.routes.js
@@ -0,0 +1,26 @@
+import Router from "express";
+import { sendMyEmail } from "../utils/sendEmail.js";
+
+const route = new Router();
+
+
+route.post("/api/sendemail", async (req, res) => {
+    const { email, title, text } = req.body;
+
+    try {
+        const send_to = process.env.EMAIL_TO;
+        const send_from = process.env.EMAIL_FROM;
+        const reply_to = process.env.EMAIL_TO;
+        const subject = title;
+        const message = `<p>${text}</p>
+        Message from: ${email}`;
+
+        await sendMyEmail(subject, message, send_to, send_from, reply_to);
+
+        res.status(200).json({ success: true, message: "Email send" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error });
+    }
+});
+
+export default route
